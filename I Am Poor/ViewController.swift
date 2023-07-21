c716f0d0efdd42f8d13ce3ab95f8901323438155
Morganzy9 <119118480+Morganzy9@users.noreply.github.com>
2023-07-21 21:41:33 +0500

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ислам Пулатов on 7/21/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
